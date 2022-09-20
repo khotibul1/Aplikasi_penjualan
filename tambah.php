495ccf76ed426f177d6e4d5501c02b0ea73be3fe
khotibul1 <109120373+khotibul1@users.noreply.github.com>
2022-09-20 15:08:21 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,30 @@
+<tlml>
+<had>
+</head>
+<body>
+    <h1>Data Master Barang</h1>
+    <form action="proses_tambah.php" method="POST">
+        <fielset>
+            <p>
+                <label four="nama">Nama Barang :</label>
+                <input type="Text" name="nama"/>
+            </p>
+            <p>
+                <label for="stok">Stok Barang :</label>
+                <input type="number" name="stok" />
+            </p>
+            <p>
+            <label for="harga">harga Barang :</label>
+                <input type="text" name="harga" />
+
+            </p>
+            <p>
+                <input type="submit"value="Tambah Barang" name="tambah" />
+            </p>
+        </fieldset>
+      </from>
+   </body>
+</html>
+                
+
+    
